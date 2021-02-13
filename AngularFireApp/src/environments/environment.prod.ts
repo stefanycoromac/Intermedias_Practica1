3237f01344121487e3308fadf14bd62a97c8f853
Jessica Botón <jessicaboton@gmail.com>
2021-02-13 00:56:33 -0600

    :cloud: ng build --prod
    FIRST: I also copy the firebase attribute from environment.ts to environment.prod.ts
    SECOND: the command execution

--- AngularFireApp/src/environments/environment.prod.ts
+++ AngularFireApp/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    //Contenido de la variable firebaseConfig al crear la BD
+    apiKey: "AIzaSyCFkRlEyrVqwGm7jlDkNhVffeSa83veqqg",
+    authDomain: "intermediasprac1angularfireapp.firebaseapp.com",
+    projectId: "intermediasprac1angularfireapp",
+    storageBucket: "intermediasprac1angularfireapp.appspot.com",
+    messagingSenderId: "430533664382",
+    appId: "1:430533664382:web:dc5f8dfe96d2a046d3822f",
+    measurementId: "G-RG0L03STKR"
+  }
 };
